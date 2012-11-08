0c49792a43a3cfb5bd8f3daf65b93530a1ef53cd
Yoni Tsafir <yoni@joytunes.com>
2012-11-08 17:52:32 +0200

    Re-allowed FBLoginDialog as fallback so we can test in simulator
    For this to work, you need to apply the following patch in FBDialog's code:
    http://stackoverflow.com/questions/12712824/facebook-login-dialog-does-not-work-on-ios6-simulator

--- Classes/ShareKit/Sharers/Services/Facebook/SHKFacebook.m
+++ Classes/ShareKit/Sharers/Services/Facebook/SHKFacebook.m
@@ -164,7 +164,7 @@ static SHKFacebook *requestingPermisSHKFacebook=nil;
         (session.state == FBSessionStateCreatedTokenLoaded)) {
 		[[SHKActivityIndicator currentIndicator] displayActivity:SHKLocalizedString(@"Logging In...")];
         [FBSession setActiveSession:session];
-        [session openWithBehavior:FBSessionLoginBehaviorWithNoFallbackToWebView
+        [session openWithBehavior:FBSessionLoginBehaviorWithFallbackToWebView
 				completionHandler:^(FBSession *session, FBSessionState state, NSError *error) {
 					[[SHKActivityIndicator currentIndicator] hide];
 					[self sessionStateChanged:session state:state error:error];
